971bead41ddda2adc2578118b9dd6a7b7d00869c
edgars <edgarsma@gmail.com>
2018-06-08 17:56:35 +0300

    newline listener

--- app/src/main/java/com/plantmer/soilsensor/Fragment/MainFragment.java
+++ app/src/main/java/com/plantmer/soilsensor/Fragment/MainFragment.java
@@ -3,7 +3,6 @@ package com.plantmer.soilsensor.Fragment;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.AppBarLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,42 +12,28 @@ import android.widget.TextView;
 
 import com.plantmer.soilsensor.MainActivity;
 import com.plantmer.soilsensor.R;
-import com.plantmer.soilsensor.util.Utils;
-
-import java.util.Locale;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements View.OnClickListener{
 
     MainActivity act;
     public MainFragment() {
         // Required empty public constructor
     }
 
-    private String mIconWind;
-    private String mIconHumidity;
-    private String mIconPressure;
-    private String mIconCloudiness;
-    private String mIconSunrise;
-    private String mIconSunset;
-    private String mPercentSign;
-    private String mPressureMeasurement;
 
-    private TextView mPressureView;
-    private TextView mCloudinessView;
-    private TextView mSunriseView;
-    private TextView mSunsetView;
-    private TextView mIconPressureView;
-    private TextView mIconCloudinessView;
-    private TextView mIconSunriseView;
-    private TextView mIconSunsetView;
+    private TextView mDielectricPermittivityView;
+    private TextView mElectricalConductivityView;
+    private TextView mTemperatureView;
+    private TextView mVWCView;
+
     private TextView connText;
-
     private Button connButton;
 
+    private String mPercentSign;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -58,68 +43,46 @@ public class MainFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState){
-        weatherConditionsIcons();
-        initializeTextView(view);
+        initializeTextView();
     }
 
     private void updateCurrentWeather() {
-        String pressure = String.format(Locale.getDefault(), "%.1f",10.01f);
-        String wind = String.format(Locale.getDefault(), "%.1f", 10.01f);
-
-
-        mPressureView.setText(getString(R.string.pressure_label, pressure,
-                mPressureMeasurement));
-        mSunriseView.setText(getString(R.string.sunrise_label, "19:00"));
-        mSunsetView.setText(getString(R.string.sunset_label, "19:00"));
+        mDielectricPermittivityView.setText(getString(R.string.dp_label, "20.0"));
+        mElectricalConductivityView.setText(getString(R.string.ec_label, "20.0"));
+        mTemperatureView.setText(getString(R.string.temperature_with_degree, "20.0"));
+        mVWCView.setText(getString(R.string.humidity_label, "10.0", mPercentSign));
 
      }
-
-    private void initializeTextView(View vv) {
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId()) {
+            case R.id.connButton:
+                // Do something
+        }
+    }
+    private void initializeTextView() {
+        mPercentSign = getString(R.string.percent_sign);
         Typeface weatherFontIcon = Typeface.createFromAsset(getActivity().getAssets(),
                 "fonts/weathericons-regular-webfont.ttf");
         Typeface robotoLight = Typeface.createFromAsset(getActivity().getAssets(),
                 "fonts/Roboto-Light.ttf");
 
         connButton =  getActivity().findViewById(R.id.connButton);
+        connButton.setOnClickListener(this);
         connText =  getActivity().findViewById(R.id.connText);
 
-        mPressureView =  getActivity().findViewById(R.id.main_pressure);
-        mCloudinessView =  getActivity().findViewById(R.id.main_cloudiness);
-        mSunriseView =  getActivity().findViewById(R.id.main_sunrise);
-        mSunsetView =  getActivity().findViewById(R.id.main_sunset);
+        mDielectricPermittivityView =  getActivity().findViewById(R.id.main_dp);
+        mElectricalConductivityView =  getActivity().findViewById(R.id.main_ec);
+        mTemperatureView =  getActivity().findViewById(R.id.main_temp);
+        mVWCView =  getActivity().findViewById(R.id.main_vwc);
 
         connButton.setTypeface(robotoLight);
         connText.setTypeface(robotoLight);
-        mPressureView.setTypeface(robotoLight);
-        mCloudinessView.setTypeface(robotoLight);
-        mSunriseView.setTypeface(robotoLight);
-        mSunsetView.setTypeface(robotoLight);
+        mDielectricPermittivityView.setTypeface(robotoLight);
+        mElectricalConductivityView.setTypeface(robotoLight);
+        mTemperatureView.setTypeface(robotoLight);
+        mVWCView.setTypeface(robotoLight);
 
-        /**
-         * Initialize and configure weather icons
-         */
-        mIconPressureView =  getActivity().findViewById(R.id.main_pressure_icon);
-        mIconPressureView.setTypeface(weatherFontIcon);
-        mIconPressureView.setText(mIconPressure);
-        mIconCloudinessView =  getActivity().findViewById(R.id.main_cloudiness_icon);
-        mIconCloudinessView.setTypeface(weatherFontIcon);
-        mIconCloudinessView.setText(mIconCloudiness);
-        mIconSunriseView =  getActivity().findViewById(R.id.main_sunrise_icon);
-        mIconSunriseView.setTypeface(weatherFontIcon);
-        mIconSunriseView.setText(mIconSunrise);
-        mIconSunsetView =  getActivity().findViewById(R.id.main_sunset_icon);
-        mIconSunsetView.setTypeface(weatherFontIcon);
-        mIconSunsetView.setText(mIconSunset);
-    }
-
-    private void weatherConditionsIcons() {
-        mIconWind = getString(R.string.icon_wind);
-        mIconHumidity = getString(R.string.icon_humidity);
-        mIconPressure = getString(R.string.icon_barometer);
-        mIconCloudiness = getString(R.string.icon_cloudiness);
-        mPercentSign = getString(R.string.percent_sign);
-        mPressureMeasurement = getString(R.string.pressure_measurement);
-        mIconSunrise = getString(R.string.icon_sunrise);
-        mIconSunset = getString(R.string.icon_sunset);
     }
 }
